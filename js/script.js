2cd1af30ec58942ee6da275e14f00ed73dab2fc4
mn46 <m.nowak033@gmail.com>
2021-11-21 12:59:00 +0100

    Added animations in menu. Added background music. Changed appearance and number of elements.

--- js/script.js
+++ js/script.js
@@ -3,6 +3,10 @@
 let lives = 5;
 let points = 0;
 let time_left = 60;
+let gameMusic = document.querySelector("#game_music");
+let menuMusic = document.querySelector("#menu_music");
+let loseMusic = document.querySelector("#lose_music");
+let winMusic = document.querySelector("#win_music");
 
 // time
 
@@ -21,23 +25,47 @@ function startTimer() {
     }
 }
 
-window.addEventListener("load", main);
+window.addEventListener("load", firstClick);
 sound();
+
+// pre-start
+
+function firstClick() {
+    document.querySelector("#pre-start").addEventListener("click", main)
+}
 
 // /* sound button */
 
 function sound() {
-document.querySelector("#container_sound").classList.add("sound_on");
-document.querySelector("#container_sound").addEventListener("click", soundOff);
+document.querySelector("#sound_button").addEventListener("click", soundOff);
 }
 
 function soundOff() {
     console.log("soundOff");
-    document.querySelector("#container_sound").classList.toggle("sound_off");
+    this.removeEventListener("click", soundOff)
+    this.classList.remove("sound_on");
+    this.classList.add("sound_off");
+    this.addEventListener("click", soundOn);
+    gameMusic.muted = true;
+}
+
+function soundOn() {
+    console.log("soundOn");
+    this.removeEventListener("click", soundOn)
+    this.classList.remove("sound_off");
+    this.classList.add("sound_on");
+    this.addEventListener("click", soundOff);
+    gameMusic.muted = false;
 }
 
 // main menu
 function main() {
+    this.removeEventListener("click", main)
+    this.classList.add("hidden");
+    document.querySelector("#menu").classList.remove("hidden");
+    document.querySelector("#black").classList.add("lights");
+    document.querySelector("#courtain").classList.add("courtain");
+    playMenumusic();
     document.querySelector("#play_button").addEventListener("click", menuStart);
     document.querySelector("#instruction_play_button").addEventListener("click", menuStart);
     document.querySelector("#instructions_button").addEventListener("click", showInstructions);
@@ -53,6 +81,23 @@ function showInstructions() {
     document.querySelector("#instruction_play_button").classList.remove("hidden");
 }
 
+// playing menu music from beginning
+
+function playMenumusic() {
+    menuMusic.currentTime = 0;
+    menuMusic.play();
+}
+
+// playing game music from the beginning
+
+function playMusic() {
+    gameMusic.currentTime = 0;
+    gameMusic.play();
+}
+
+function stopMenumusic() {
+    menuMusic.muted = true
+}
 
 // starting the game
 
@@ -64,30 +109,38 @@ function start() {
     showTimer ();
     setPosition();
 
+    stopMenumusic();
+    playMusic();
+
     document.querySelector("#container_phone").addEventListener("animationiteration", randomPosition);
+    document.querySelector("#container_phone2").addEventListener("animationiteration", randomPosition);
+    document.querySelector("#container_phone3").addEventListener("animationiteration", randomPosition);
     document.querySelector("#container_applause").addEventListener("animationiteration", randomPosition);
-    document.querySelector("#container_angry").addEventListener("animationiteration", randomPosition);
-    
+    document.querySelector("#container_applause2").addEventListener("animationiteration", randomPosition);
+    document.querySelector("#container_applause3").addEventListener("animationiteration", randomPosition);
 
     // animated objects
 
     document.querySelector("#sprite_phone").classList.add("fading_objects");
+    document.querySelector("#sprite_phone2").classList.add("fading_objects");
+    document.querySelector("#sprite_phone3").classList.add("fading_objects");
     document.querySelector("#sprite_applause").classList.add("fading_objects");
-    document.querySelector("#sprite_angry").classList.add("fading_objects");
-
-    // missing phone
-
-    document.querySelector("#container_phone").addEventListener("animationiteration", missPhone);
-
-    // clicking good objects
-
-    document.querySelector("#container_phone").addEventListener("click", clickPhone);
-
+    document.querySelector("#sprite_applause2").classList.add("fading_objects");
+    document.querySelector("#sprite_applause3").classList.add("fading_objects");
 
     // clicking bad objects
 
-    document.querySelector("#container_applause").addEventListener("click", clickBadobject);
-    document.querySelector("#container_angry").addEventListener("click", clickBadobject);
+    document.querySelector("#container_phone").addEventListener("click", clickPhone);
+    document.querySelector("#container_phone2").addEventListener("click", clickPhone);
+    document.querySelector("#container_phone3").addEventListener("click", clickPhone);
+
+
+    // clicking good objects
+
+    document.querySelector("#container_applause").addEventListener("click", clickClap);
+    document.querySelector("#container_applause2").addEventListener("click", clickClap);
+    document.querySelector("#container_applause3").addEventListener("click", clickClap);
+
 }
 
 // objects - setting position
@@ -99,8 +152,11 @@ function randomNumber() {
 
 function setPosition() {
     document.querySelector("#container_phone").classList.add(randomNumber());
+    document.querySelector("#container_phone2").classList.add(randomNumber());
+    document.querySelector("#container_phone3").classList.add(randomNumber());
     document.querySelector("#container_applause").classList.add(randomNumber());
-    document.querySelector("#container_angry").classList.add(randomNumber());
+    document.querySelector("#container_applause2").classList.add(randomNumber());
+    document.querySelector("#container_applause3").classList.add(randomNumber());
 }
 
 // objects - showing randomly
@@ -110,22 +166,41 @@ function randomPosition() {
     this.classList.add(randomNumber());
 }
 
+// preventing objects from appearing one on another
+
+const positions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+for (let i = 0; i < positions.length; i++) {
+
+}
+
+// objects - animations and behavior
+
+function clickClap() {
+    this.removeEventListener("click", clickClap);
+    this.firstElementChild.classList.remove("fading_objects");
+    this.firstElementChild.classList.add("good_click");
+    this.firstElementChild.addEventListener("animationend", restartClap);
+    addPoint();
+}
 
 function clickPhone() {
     this.removeEventListener("click", clickPhone);
     this.firstElementChild.classList.remove("fading_objects");
-    this.firstElementChild.classList.add("good_click");
-    this.firstElementChild.addEventListener("animationend", restartPhone);
-    addPoint();
-}
-
-function clickBadobject() {
-    this.removeEventListener("click", clickBadobject);
-    this.firstElementChild.classList.remove("fading_objects");
     this.firstElementChild.classList.add("bad_click");
-    this.firstElementChild.addEventListener("animationend", restartBadelement);
+    this.firstElementChild.addEventListener("animationend", restartPhone);
+    angryPavarotti();
     removeHealth();
 }
+
+// when phone is clicked
+
+function angryPavarotti() {
+    document.querySelector("#pavarotti").classList.remove("sprite_pavarotti");
+    document.querySelector("#pavarotti").classList.add("angry_pavarotti");
+}
+
+// points and health
 
 function addPoint() {
     points++;
@@ -149,31 +224,30 @@ function restartPhone() {
     console.log("restartPhone");
     this.parentNode.classList.add(randomNumber());
     this.parentNode.addEventListener("animationiteration", randomPosition);
-    this.classList.remove("good_click");
+    this.classList.remove("bad_click");
     this.classList.add("fading_objects");
     this.parentNode.addEventListener("click", clickPhone);
+    document.querySelector("#pavarotti").classList.remove("angry_pavarotti");
+    document.querySelector("#pavarotti").classList.add("sprite_pavarotti");
 }
 
 // restart bad element
 
-function restartBadelement() {
-    console.log("restartBadelement");
+function restartClap() {
+    console.log("restartClap");
     this.parentNode.classList.add(randomNumber());
     this.parentNode.addEventListener("animationiteration", randomPosition);
-    this.classList.remove("bad_click");
+    this.classList.remove("good_click");
     this.classList.add("fading_objects");
-    this.parentNode.addEventListener("click", clickBadobject);
+    this.parentNode.addEventListener("click", clickClap);
 }
 
-function missPhone() {
-    console.log("missPhone");
-    this.firstElementChild.addEventListener("animationend", restartBadelement);
-    removeHealth();
-}
+// winning game and reply
 
 function winGame() {
     console.log("winGame");
     endGame();
+    winMusic.play();
     document.querySelector("#win").classList.remove("hidden");
     document.querySelector("#win_illustration").classList.remove("hidden");
     document.querySelector("#win_text").classList.remove("hidden");
@@ -190,15 +264,29 @@ function winReplay() {
     document.querySelector("#heart2").classList.add("heart");
     document.querySelector("#heart3").classList.add("heart");
     document.querySelector("#heart4").classList.add("heart");
+    document.querySelector("#point1").classList.remove("point");
+    document.querySelector("#point2").classList.remove("point");
+    document.querySelector("#point3").classList.remove("point");
+    document.querySelector("#point4").classList.remove("point");
+    document.querySelector("#point5").classList.remove("point");
+    document.querySelector("#point6").classList.remove("point");
+    document.querySelector("#point7").classList.remove("point");
+    document.querySelector("#point8").classList.remove("point");
+    document.querySelector("#point9").classList.remove("point");
+    document.querySelector("#point10").classList.remove("point");
     lives = 5;
     points = 0;
-    time = 60;
+    time_left = 60;
+    gameMusic.muted = false;
     start();
 }
+
+// losing game
 
 function loseGame() {
     console.log("loseGame");
     endGame();
+    loseMusic.play();
     document.querySelector("#lose").classList.remove("hidden");
     document.querySelector("#lose_illustration").classList.remove("hidden");
     document.querySelector("#lose_text").classList.remove("hidden");
@@ -215,79 +303,29 @@ function loseReplay() {
     document.querySelector("#heart2").classList.add("heart");
     document.querySelector("#heart3").classList.add("heart");
     document.querySelector("#heart4").classList.add("heart");
+    document.querySelector("#point1").classList.remove("point");
+    document.querySelector("#point2").classList.remove("point");
+    document.querySelector("#point3").classList.remove("point");
+    document.querySelector("#point4").classList.remove("point");
+    document.querySelector("#point5").classList.remove("point");
+    document.querySelector("#point6").classList.remove("point");
+    document.querySelector("#point7").classList.remove("point");
+    document.querySelector("#point8").classList.remove("point");
+    document.querySelector("#point9").classList.remove("point");
+    document.querySelector("#point10").classList.remove("point");
     lives = 5;
     points = 0;
-    time = 60;
+    time_left = 60;
+    gameMusic.muted = false;
     start();
 }
 
 function endGame() {
+    gameMusic.muted = true;
     document.querySelector("#container_phone").removeEventListener("animationiteration", randomPosition);
+    document.querySelector("#container_phone2").removeEventListener("animationiteration", randomPosition);
+    document.querySelector("#container_phone3").removeEventListener("animationiteration", randomPosition);
     document.querySelector("#container_applause").removeEventListener("animationiteration", randomPosition);
-    document.querySelector("#container_angry").removeEventListener("animationiteration", randomPosition);
-    document.querySelector("#container_phone").removeEventListener("animationiteration", missPhone);
+    document.querySelector("#container_applause2").removeEventListener("animationiteration", randomPosition);
+    document.querySelector("#container_applause3").removeEventListener("animationiteration", randomPosition);
 }
-
-
-
-
-
-
-
-
-
-
-
-// /* variables */
-
-// const sprites = ["#container_phone", "#container_applause", "#container_angry"];
-// let objects = Math.floor(Math.random() * sprites.length);
-// let random_id = sprites[objects];
-
-// let objects_number = Math.floor(Math.random() * 6 + 1);
-
-
-
-// /* objects appearing in random places */
-
-// function random_objects() {
-//     document.querySelector(random_id).classList.add("position" + objects_number);
-//     console.log(objects_number + " " + random_id);
-// }
-
-
-// function remove_position() {
-//     document.querySelector(random_id).classList.remove("position" + objects_number)
-//     console.log("remove_position");
-// }
-
-// // document.querySelector("#container_phone").classList.add("position" + objects_number);
-
-// // document.querySelector("#container_applause").classList.add("position" + objects_number);
-
-// // document.querySelector("#container_angry").classList.add("position" + objects_number);
-
-// setInterval(random_objects, 4000);
-
-// if()
-// remove_position();
-
-// /* clicking good objects */
-
-// // function good_object() {
-// //     // play animation
-// //     // add a point
-// //     document.querySelector("#points_container").classList.add("point" + )
-// // }
-
-// // document.querySelector("#container_phone").addEventListener("click", good_object);
-
-// // /* clicking bad objects */
-
-// // function bad_object() {
-// //     // play animation
-// //     // subtract one life
-// // }
-
-// // document.querySelector("#container_applause").addEventListener("click", bad_object);
-// // document.querySelector("#container_angry").addEventListener("click", bad_object);
